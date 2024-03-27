f345b55e0ac9ad9e56100fe23bba8d85a78a5bef
Filipe Machado <x.filipe.machado.x@gmail.com>
2024-03-27 16:25:47 -0300

    created project

---
+++ src/main/java/com/xflprflx/paycheck/PaycheckApplication.java
@@ -0,0 +1,13 @@
+package com.xflprflx.paycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaycheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaycheckApplication.class, args);
+	}
+
+}
