62c578a09f95305183e72070c2991411c100e705
Filipe Machado <x.filipe.machado.x@gmail.com>
2024-04-18 13:22:50 -0300

    corrige problema de casas decimais

--- src/main/java/com/xflprflx/paycheck/services/CsvFileProcessor.java
+++ src/main/java/com/xflprflx/paycheck/services/CsvFileProcessor.java
@@ -8,15 +8,14 @@ import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.csv.QuoteMode;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CsvFileProcessor implements FileProcessor {
 
@@ -57,22 +56,44 @@ public class CsvFileProcessor implements FileProcessor {
         return invoices;
     }
 
+
     @Override
     public List<TransportDocumentDTO> returnTransportDocumentListFromFile(MultipartFile file) throws IOException {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         InputStream inputStream = file.getInputStream();
         List<TransportDocumentDTO> transportDocuments = new ArrayList<>();
-        try (CSVParser parser = new CSVParser(new InputStreamReader(inputStream), CSVFormat.DEFAULT.withDelimiter(';').withQuoteMode(QuoteMode.MINIMAL))) {
-            for (CSVRecord record : parser) {
-                System.out.println(record);
-                if (record.getRecordNumber() != 1) {
-                    String[] fields = record.get(0).split(";");
-                    String number = fields.length > 0 ? fields[0] : null;
-                    String serie = fields.length > 1 && !fields[1].isEmpty() ? fields[1] : null;
-                    LocalDate issueDate = fields.length > 2 && !fields[2].isEmpty() ? LocalDate.parse(fields[2].replaceAll("^\"|\"$", "").split(" ")[0], formatter) : null;
-                    String addressShipper = fields.length > 3 && !fields[3].isEmpty() ? fields[3].replaceAll("^\"|\"$", "") : null;
-                    Double amount = fields.length > 4 && !fields[4].isEmpty() ? Double.parseDouble(fields[4]) : null;
-                    String[] invoices = fields.length >5 && !fields[5].isEmpty() ? fields[5].split(", ") : null;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+             CSVParser parser = new CSVParser(bufferedReader,
+                     CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader())) {
+
+            List<String> lines = bufferedReader.lines().collect(Collectors.toList());
+            List<String> processedLines = new ArrayList<>();
+
+// Processar as linhas para substituir vírgulas por pontos em valores numéricos
+            for (String line : lines) {
+                processedLines.add(line.replaceAll("(?<=\\d),(?=\\d{2}(?:\\.)?\\d+)", "."));
+            }
+
+
+            // Recriar o BufferedReader com as linhas processadas
+            try (BufferedReader processedReader = new BufferedReader(new StringReader(String.join(System.lineSeparator(), processedLines)))) {
+                CSVParser parser2 = new CSVParser(processedReader,
+                        CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader());
+
+                for (CSVRecord record : parser2) {
+                    System.out.println(record);
+                    // Processar cada registro e criar objetos TransportDocumentDTO
+                    String number = record.get(0);
+                    String serie = record.get(1);
+                    LocalDate issueDate = LocalDate.parse(record.get(2).split(" ")[0], formatter);
+                    String addressShipper = record.get(3);
+
+                    System.out.println(record.get(4));
+//                    Double amount = Double.parseDouble(record.get(4).replace(".", ""));
+                    Double amount = (Double.valueOf(record.get(4).toString().replaceAll("\\.", "").replace(",", ".")));
+                    System.out.println(amount);
+                    String[] invoices = record.get(5).split(", ");
 
                     TransportDocumentDTO transportDocument = new TransportDocumentDTO();
                     transportDocument.setNumber(number);
@@ -80,19 +101,31 @@ public class CsvFileProcessor implements FileProcessor {
                     transportDocument.setIssueDate(issueDate);
                     transportDocument.setAddressShipper(addressShipper);
                     transportDocument.setAmount(amount);
-                    /*Arrays.stream(invoices).forEach(x -> {
+                    System.out.println(transportDocument.getAmount());
+                    Arrays.stream(invoices).forEach(x -> {
                         var inv = new InvoiceDTO();
                         inv.setNumber(x);
                         transportDocument.getInvoices().add(inv);
-                    });*/
+                    });
                     transportDocuments.add(transportDocument);
                 }
             }
-        } catch (DateTimeParseException e) {
-            // Tratar exceção de formatação inválida de data
-            System.err.println("Erro ao formatar data: " + e.getMessage());
+        } catch (IOException | DateTimeParseException e) {
+            e.printStackTrace();
         }
 
         return transportDocuments;
     }
+
+/*    // Método para converter uma string que representa um valor numérico em double
+    private Double parseAmountString(String amountStr) {
+        // Substituir vírgulas que estão entre dígitos numéricos por pontos
+        amountStr = amountStr.replaceAll("(?<=\\d),(?=\\d)", ".");
+
+        // Converter a string para double
+        return Double.parseDouble(amountStr);
+    }*/
+
 }
+
+
