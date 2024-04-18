612c7efe583c6b423a1bcd2826822e8619abcbc3
Filipe Machado <x.filipe.machado.x@gmail.com>
2024-04-18 12:02:26 -0300

    corrige problemas com aspas

--- src/main/java/com/xflprflx/paycheck/services/CsvFileProcessor.java
+++ src/main/java/com/xflprflx/paycheck/services/CsvFileProcessor.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CsvFileProcessor implements FileProcessor {
@@ -30,7 +31,6 @@ public class CsvFileProcessor implements FileProcessor {
                     String number = fields.length > 0 ? fields[0] : null;
                     LocalDate scannedDate = null;
                     LocalDate paymentApprovalDate = null;
-
                     if (fields.length > 1 && !fields[1].isEmpty()) {
                         scannedDate = LocalDate.parse(fields[1].split(" ")[0], formatter);
                     }
@@ -58,7 +58,39 @@ public class CsvFileProcessor implements FileProcessor {
 
     @Override
     public List<TransportDocumentDTO> returnTransportDocumentListFromFile(MultipartFile file) throws IOException {
-        return null;
-    }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        InputStream inputStream = file.getInputStream();
+        List<TransportDocumentDTO> transportDocuments = new ArrayList<>();
+        try (CSVParser parser = new CSVParser(new InputStreamReader(inputStream), CSVFormat.DEFAULT)) {
+            for (CSVRecord record : parser) {
+                if (record.getRecordNumber() != 1) {
+                    String[] fields = record.get(0).split(";");
+                    String number = fields.length > 0 ? fields[0] : null;
+                    String serie = fields.length > 1 && !fields[1].isEmpty() ? fields[1] : null;
+                    LocalDate issueDate = fields.length > 2 && !fields[2].isEmpty() ? LocalDate.parse(fields[2].replaceAll("^\"|\"$", "").split(" ")[0], formatter) : null;
+                    String addressShipper = fields.length > 3 && !fields[3].isEmpty() ? fields[3].replaceAll("^\"|\"$", "") : null;
+                    Double amount = fields.length > 4 && !fields[4].isEmpty() ? Double.parseDouble(fields[4]) : null;
+                    String[] invoices = fields.length >5 && !fields[5].isEmpty() ? fields[5].split(", ") : null;
 
+                    TransportDocumentDTO transportDocument = new TransportDocumentDTO();
+                    transportDocument.setNumber(number);
+                    transportDocument.setSerie(serie);
+                    transportDocument.setIssueDate(issueDate);
+                    transportDocument.setAddressShipper(addressShipper);
+                    transportDocument.setAmount(amount);
+                    /*Arrays.stream(invoices).forEach(x -> {
+                        var inv = new InvoiceDTO();
+                        inv.setNumber(x);
+                        transportDocument.getInvoices().add(inv);
+                    });*/
+                    transportDocuments.add(transportDocument);
+                }
+            }
+        } catch (DateTimeParseException e) {
+            // Tratar exceção de formatação inválida de data
+            System.err.println("Erro ao formatar data: " + e.getMessage());
+        }
+
+        return transportDocuments;
+    }
 }
